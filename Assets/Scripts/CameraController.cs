a5f31355e252b8ae3c85ec0a9ea0886244d41691
DanielDH01 <danieldenheijer14@gmail.com>
2023-03-07 16:43:53 +0100

    Added cinemachine package and added the camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] private float rotationSpeed = 100f;
+    [SerializeField] private float moveSpeed = 10f;
     // Update is called once per frame
     void Update()
     {
+        Vector3 inputMoveDir = new Vector3(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputMoveDir.z = +1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputMoveDir.z = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputMoveDir.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputMoveDir.x = +1f;
+        }
+
+
+        Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
+        transform.position += moveVector * (moveSpeed * Time.deltaTime);
+
+        Vector3 rotationVector = new Vector3(0, 0, 0);
         
+        if (Input.GetKey(KeyCode.Q))
+        {
+            rotationVector.y += 1f;
+        }
+        
+        if (Input.GetKey(KeyCode.E))
+        {
+            rotationVector.y -= 1f;
+        }
+
+        transform.eulerAngles += rotationVector * (rotationSpeed * Time.deltaTime);
     }
 }
