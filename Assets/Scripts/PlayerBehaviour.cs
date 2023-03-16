d11812a7bd18d3eb4c401b0efb11ac27aeaf233d
Yorick <yorickschmidt@ziggo.nl>
2023-03-16 15:31:36 +0100

    Added healthbars to tutorialmap, made healthbar prefab

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour
 {
-    public GameObject healthBar;
+    public GameObject kingHealthBar;
+    public GameObject viking1HealthBar;
+    public GameObject viking2HealthBar;
+    public GameObject viking3HealthBar;
 
     void Start()
     {
-        healthBar = GameObject.Find("HealthBarBack/HealthBarFront");
+        kingHealthBar = GameObject.Find("KingHealthBar/HealthBarFront");
+        viking1HealthBar = GameObject.Find("Viking1HealthBar/HealthBarFront");
+        viking2HealthBar = GameObject.Find("Viking2HealthBar/HealthBarFront");
+        viking3HealthBar = GameObject.Find("Viking3HealthBar/HealthBarFront");
     }
 
     void Update()
@@ -28,7 +34,7 @@ public class PlayerBehaviour : MonoBehaviour
     private void PlayerTakeDmg(int dmg)
     {
         GameManager.gameManager._playerHealth.DmgUnit(dmg);
-        healthBar.transform.localScale = (new Vector3(
+        kingHealthBar.transform.localScale = (new Vector3(
             GameManager.gameManager._playerHealth.HealthPercentage(GameManager.gameManager._playerHealth.Health),
             (float)1.010101, (float)1.010101));
     }
@@ -36,7 +42,7 @@ public class PlayerBehaviour : MonoBehaviour
     private void PlayerHeal(int healing)
     {
         GameManager.gameManager._playerHealth.HealUnit(healing);
-        healthBar.transform.localScale = (new Vector3(
+        kingHealthBar.transform.localScale = (new Vector3(
             GameManager.gameManager._playerHealth.HealthPercentage(GameManager.gameManager._playerHealth.Health),
             (float)1.010101, (float)1.010101));
     }
