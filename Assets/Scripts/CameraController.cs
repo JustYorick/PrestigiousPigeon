23fffb24246ba3123b7d0baa4832dca54c525f7d
Michael Scholten <michael-scholten@hotmail.nl>
2023-03-23 11:04:03 +0100

    Switched the CameraController from the old to the new input system.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,49 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed = 100f;
     [SerializeField] private float moveSpeed = 10f;
+    private Vector3 inputMoveDir = Vector3.zero;
+    private float rotation = 0.0f;
+
     // Update is called once per frame
     void Update()
     {
-        Vector3 inputMoveDir = new Vector3(0, 0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputMoveDir.z = +1f;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputMoveDir.z = -1f;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputMoveDir.x = -1f;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputMoveDir.x = +1f;
-        }
-
-
         Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
-        transform.position += moveVector * (moveSpeed * Time.deltaTime);
-
-        Vector3 rotationVector = new Vector3(0, 0, 0);
-        
-        if (Input.GetKey(KeyCode.Q))
-        {
-            rotationVector.y += 1f;
-        }
-        
-        if (Input.GetKey(KeyCode.E))
-        {
-            rotationVector.y -= 1f;
-        }
-
-        transform.eulerAngles += rotationVector * (rotationSpeed * Time.deltaTime);
+        transform.position += moveVector * moveSpeed * Time.deltaTime;
+        transform.eulerAngles += new Vector3(0, rotation, 0) * rotationSpeed * Time.deltaTime;
     }
+
+    void OnMove(InputValue value){
+        Vector2 movement = value.Get<Vector2>();
+        inputMoveDir = new Vector3(movement.x, 0f, movement.y);
+    }
+
+    void OnRotate(InputValue value) => rotation = value.Get<float>();
 }
