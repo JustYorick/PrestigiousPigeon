c9abf555ba388f5419debd6954dcd67bb8b2c045
rick <rick2.0@hotmail.nl>
2023-03-28 09:30:38 +0200

    basic structure remade

---
+++ Assets/ReDesign/SpellEffect.cs
@@ -0,0 +1,18 @@
+using System;
+using UnityEngine;
+
+namespace ReDesign
+{
+    public abstract class SpellEffect
+    {
+        public abstract void Effect();
+    }
+
+    public class Freeze : SpellEffect
+    {
+        public override void Effect()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Assets/ReDesign/StateController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace ReDesign
+{
+    public class StateController : MonoBehaviour
+    {
+        private static StateController _instance;
+        public static StateController Instance { get { return _instance; } }
+        private void Awake()
+        {
+            if (_instance != null && _instance != this)
+            {
+                Destroy(this.gameObject);
+            } else {
+                _instance = this;
+            }
+        }
+
+
+        public GameState currentState { get; private set; } = GameState.Idle;
+
+        public void ChangeState(GameState state)
+        {
+            currentState = state;
+        }
+    }
+
+    public enum GameState
+    {
+        Idle,
+        ballin,
+        offDaZoinks,
+    }
+}
