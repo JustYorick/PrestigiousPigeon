3a206547a47bdafcd2c0c55e99ae9bcec69aec70
DanielDH01 <danieldenheijer14@gmail.com>
2023-04-13 11:13:34 +0200

    Fixed the camera movement

--- Assets/Scripts/ReDesign/CameraController.cs
+++ Assets/Scripts/ReDesign/CameraController.cs
@@ -10,7 +10,6 @@ namespace ReDesign
         private static CameraController _instance;
         public static CameraController Instance { get { return _instance; } }
 
-
         private void Awake()
         {
             if (_instance != null && _instance != this)
@@ -35,24 +34,26 @@ namespace ReDesign
             // Gets the Maximal position the camera can travel in the level
             maxPos = new Vector2(WorldController.Instance.BaseLayer.Last().GameObject.transform.position.x,
                 WorldController.Instance.BaseLayer.Last().GameObject.transform.position.z);
+
         }
 
         // Update is called once per frame
         void Update()
         {
-            Debug.Log("minPos: " +minPos + "\nmaxpos: " + maxPos);
-            // Checks for position
-            // if (gameObject.transform.position.x >= minPos.x &&
-            //     gameObject.transform.position.y >= minPos.y &&
-            //     gameObject.transform.position.x <= maxPos.x &&
-            //     gameObject.transform.position.y <= maxPos.y)
-            // {
-            // }
+
                 // Calculate the direction to move the camera in, use the direction the camera is facing
                 Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
 
                 // Move the camera
                 transform.position += moveVector * (moveSpeed * Time.deltaTime);
+                
+                //Sets position to the current position if the position between the boundaries
+                gameObject.transform.position = new Vector3 
+                (
+                    Mathf.Clamp (gameObject.transform.position.x, minPos.x, maxPos.x), 
+                    0.0f, 
+                    Mathf.Clamp (gameObject.transform.position.z, minPos.y, maxPos.y)
+                );
 
                 // Rotate the camera
                 transform.eulerAngles += new Vector3(0, rotation, 0) * (rotationSpeed * Time.deltaTime);
@@ -61,52 +62,7 @@ namespace ReDesign
         void OnMove(InputValue value){
             // Get the movement input
             Vector2 movement = value.Get<Vector2>();
-            if (gameObject.transform.position.x >= minPos.x &&
-                gameObject.transform.position.y >= minPos.y &&
-                gameObject.transform.position.x <= maxPos.x &&
-                gameObject.transform.position.y <= maxPos.y)
-            {
-                // Use the x and y for movement on the x and z axis, respectively
-                inputMoveDir = new Vector3(movement.x, 0f, movement.y);
-            }else{
-                // check if camera empty is in topright
-                if (gameObject.transform.position.x >= maxPos.x
-                    && gameObject.transform.position.y >= maxPos.y)
-                {
-                    Debug.Log("in topRight");
-                    if (movement is { x: < 0f, y: < 0f })
-                        inputMoveDir = new Vector3(movement.x, 0f, movement.y);
-                }
-                // check if camera empty is in bottomLeft
-                else if (gameObject.transform.position.x <= minPos.x
-                         && gameObject.transform.position.y <= minPos.y)
-                {
-                    Debug.Log("in bottomLeft");
-
-                    if (movement is { x: > 0f, y: > 0f })
-                        inputMoveDir = new Vector3(movement.x, 0f, movement.y);
-                }
-                // check if camera empty is in topLeft
-                else if (gameObject.transform.position.x <= minPos.x
-                         && gameObject.transform.position.y >= maxPos.y)
-                {
-                    Debug.Log("in topLeft");
-
-                    if (movement is { x: > 0f, y: < 0f })
-                        inputMoveDir = new Vector3(movement.x, 0f, movement.y);
-                }
-                // check if camera empty is in bottomRight
-                else if (gameObject.transform.position.x >= maxPos.x
-                         && gameObject.transform.position.y <= minPos.y)
-                {
-                    if (movement is { x: < 0f, y: > 0f })
-                        inputMoveDir = new Vector3(movement.x, 0f, movement.y);
-                }
-            }
-
-
-
-
+            inputMoveDir = new Vector3(movement.x, 0f, movement.y);
         }
 
         // Get the rotation input
