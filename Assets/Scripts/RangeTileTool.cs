d72840dca0fd26dc94343c8e9d8106b6a343c46b
rick <rick2.0@hotmail.nl>
2023-04-17 10:18:54 +0200

    added clear tilemap method and added rangetiles to new layer

--- Assets/Scripts/RangeTileTool.cs
+++ Assets/Scripts/RangeTileTool.cs
@@ -11,6 +11,8 @@ public class RangeTileTool : MonoBehaviour
 {
     [SerializeField] private Tilemap tileMap;
     
+    [SerializeField] private Tilemap rangeTileMap;
+    
     [SerializeField] private RuleTile tile;
     
     public static RangeTileTool Instance { get; private set; }
@@ -31,10 +33,26 @@ public class RangeTileTool : MonoBehaviour
     public void SpawnTile(int xPos, int yPos, Color color)
     {
         var walkingLayer = tileMap;
-        var node = WorldController.Instance.BaseLayer.Where(t => t.XPos == xPos && t.YPos == yPos).FirstOrDefault();
+        var rangeTileTileMap = rangeTileMap;
         
+        DefaultTile node;
+        node = null;
+        foreach (var t in WorldController.Instance.BaseLayer)
+        {
+            if (t.XPos == xPos && t.YPos == yPos)
+            {
+                node = t;
+                break;
+            }
+        }
+
         Vector3Int cell = walkingLayer.WorldToCell(new Vector3(node.GameObject.transform.position.x, 0, node.GameObject.transform.position.z));
-        walkingLayer.SetTile(cell, tile);
-        walkingLayer.SetColor(cell, color);
+        rangeTileTileMap.SetTile(cell, tile);
+        rangeTileTileMap.SetColor(cell, color);
+    }
+
+    public void clearTileMap()
+    {
+        rangeTileMap.ClearAllTiles();
     }
 }
