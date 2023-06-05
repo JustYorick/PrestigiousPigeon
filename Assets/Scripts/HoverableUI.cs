65510bc8e6bead8c45cb6af4f4ca61555e7c8fa5
Yorick <yorickschmidt@ziggo.nl>
2023-06-05 16:04:03 +0200

    removed hoverable ui debugs

--- Assets/Scripts/HoverableUI.cs
+++ Assets/Scripts/HoverableUI.cs
@@ -13,13 +13,11 @@
 
      public void OnPointerEnter(PointerEventData eventData)
      {
-         Debug.Log("Button enter");
          gameObject.transform.localScale = defaultScale * 1.1f;
      }
  
      public void OnPointerExit(PointerEventData eventData)
      {
-         Debug.Log("Button exit");
          gameObject.transform.localScale = defaultScale;
      }
  }
