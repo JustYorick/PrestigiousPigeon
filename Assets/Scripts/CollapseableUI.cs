89ee61319f5f65970d02da058a3ec9c74e14b4a3
DanielDH01 <danieldenheijer14@gmail.com>
2023-06-22 12:23:22 +0200

    Revert "Merge branch 'dev' into mob_animations"
    This reverts commit b2e78431132f6eecec2818794fa40db7f600ee37, reversing
    changes made to 22b72457c1501ccbaa1a5c051d79b48b8b3f676d.

--- Assets/Scripts/CollapseableUI.cs
+++ Assets/Scripts/CollapseableUI.cs
@@ -31,20 +31,11 @@ public class CollapseableUI : MonoBehaviour
          Coroutine b = StartCoroutine(MoveUpAndDown(170 * canvas.scaleFactor, EnemyTurnUI));
     }
 
-    public void ShowObjectiveUI()
-    {
-        StartCoroutine(MoveUpAndDown(360 * canvas.scaleFactor, ObjectiveUI));
-        ObjectiveUI.transform.position = ObjectiveUIDefaultPos;
-    }
-    
     IEnumerator MoveUpAndDown(float distance, GameObject uiElement)
     {
         yield return MoveUI(distance, uiElement);
         yield return new WaitForSeconds(1);
         yield return MoveUI(-distance, uiElement);
-        if(uiElement == ObjectiveUI){
-            ReDesign.TurnController.ResolveNextTurn();
-        }
     }
     
     private IEnumerator MoveUI (float distance, GameObject uiElement)
