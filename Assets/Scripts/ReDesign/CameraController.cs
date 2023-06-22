ca8b53b7732cf33e2656f122756610abd2c6d1c8
DanielDH01 <danieldenheijer14@gmail.com>
2023-06-22 10:22:05 +0200

    Changed cameracontroller so that it will lock to player with spacebar

--- Assets/Scripts/ReDesign/CameraController.cs
+++ Assets/Scripts/ReDesign/CameraController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿﻿using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -22,17 +23,23 @@ namespace ReDesign
         
         [SerializeField] private float rotationSpeed = 100f;
         [SerializeField] private float moveSpeed = 5f;
+        [SerializeField] private GameObject playerObject;
+        [SerializeField] private Vector2 ExtraAmountToMove;
         private Vector3 inputMoveDir { get; set; } = Vector3.zero;
         private float rotation { get; set; } = 0.0f;
         private Vector2 MinPos { get; set; } = new Vector2(0f,0f);
         private Vector2 MaxPos { get; set; } = new Vector2(0f,0f);
-        [SerializeField] private Vector2 ExtraAmountToMove;
+        private bool lockedToPlayer = false;
         
+
         private void Start()
         {
             List<DefaultTile> baseLayer = WorldController.Instance.BaseLayer;
             GameObject firstGameObject = baseLayer.First().GameObject;
             GameObject lastGameObject = baseLayer.Last().GameObject;
+            
+            if(!playerObject)
+                playerObject = GameObject.Find("Player");
 
             Vector3 minimalPosition = firstGameObject.transform.position;
             Vector3 maximalPosition = lastGameObject.transform.position;
@@ -42,18 +49,30 @@ namespace ReDesign
 
         private void Update()
         {
-            // Calculate the direction to move the camera in, use the direction the camera is facing
-            Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
+            if (Input.GetKeyDown(KeyCode.Space))
+                lockedToPlayer = !lockedToPlayer;
 
-            // Move the camera
-            transform.position += moveVector * (moveSpeed * Time.deltaTime);
-            transform.eulerAngles += new Vector3(0, rotation, 0) * (rotationSpeed * Time.deltaTime);            
-            transform.position = new Vector3 
-            (
-                Mathf.Clamp (transform.position.x, MinPos.x - ExtraAmountToMove.x, MaxPos.x + ExtraAmountToMove.x), 
-                0.0f, 
-                Mathf.Clamp (transform.position.z, MinPos.y - ExtraAmountToMove.y, MaxPos.y + ExtraAmountToMove.y)
-            );
+            if (lockedToPlayer){
+                transform.position = playerObject.transform.position;
+                transform.eulerAngles += new Vector3(0, rotation, 0) * (rotationSpeed * Time.deltaTime);
+            }else if (!lockedToPlayer) {
+                // Calculate the direction to move the camera in, use the direction the camera is facing
+                Vector3 moveVector = transform.forward * inputMoveDir.z + transform.right * inputMoveDir.x;
+
+                // Move the camera
+                transform.position += moveVector * (moveSpeed * Time.deltaTime);
+                transform.eulerAngles += new Vector3(0, rotation, 0) * (rotationSpeed * Time.deltaTime);
+                transform.position = new Vector3
+                (
+                    Mathf.Clamp(transform.position.x, MinPos.x - ExtraAmountToMove.x, MaxPos.x + ExtraAmountToMove.x),
+                    transform.position.y,
+                    Mathf.Clamp(transform.position.z, MinPos.y - ExtraAmountToMove.y, MaxPos.y + ExtraAmountToMove.y)
+                );
+
+                if (Input.GetKeyDown(KeyCode.Space))
+                    transform.position = new Vector3(playerObject.transform.position.x, -1f,
+                        playerObject.transform.position.z);
+            }
         }
 
         void OnMove(InputValue value){
@@ -64,5 +83,23 @@ namespace ReDesign
 
         // Get the rotation input
         void OnRotate(InputValue value) => rotation = value.Get<float>();
+        
+        public IEnumerator MoveSmoothlyTo(Vector3 destination, float timeToMove) {
+            float totalMovementTime = timeToMove; // The amount of time you want the movement to take
+            float currentMovementTime = 0f;// The amount of time that has passed
+            Vector3 origin = transform.position; // First position of camera
+            while (Vector3.Distance(transform.localPosition, destination) > 0.2f) {
+                currentMovementTime += Time.deltaTime;
+                transform.localPosition = Vector3.Lerp(origin, destination, currentMovementTime / totalMovementTime);
+                Debug.Log("infinite looooop");
+                yield return null;
+            }
+        }
+
+        public void MoveTo(Vector3 position)
+        {
+            Debug.Log("Moving camera");
+            transform.position = position;
+        }
     }
 }
