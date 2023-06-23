58db0588f61461c87d87cab544a48fa7667c9aab
Yorick Schmidt <20132921@student.hhs.nl>
2023-06-23 18:58:23 +0200

    trying to make fitting text and borders appear when clicking on next button

--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -8,32 +8,45 @@ public class TutorialController : MonoBehaviour
 {
 
     private Canvas _tutorialCanvas;
+    private Canvas _introCanvas;
     private Canvas _statsCanvas;
     private Canvas _buttonsCanvas;
     private Canvas _spellsCanvas;
     private Canvas _infoCanvas;
-    private Button _statsNext;
-    private Button _buttonsNext;
-    private Button _spellsNext;
-    private Button _infoNext;
     private int _onClickCount;
-    
+    private GameObject _healthImages;
+    private GameObject _manaImages;
+    private GameObject _moveButtonImages;
+    private GameObject _spellButtonImages;
+    private GameObject _endTurnButtonImages;
+    private GameObject _spellsImages;
+    private GameObject _spellsInfoImages;
+    private GameObject _healthText;
+    private GameObject _manaText;
+
     void Awake()
     {
-        _statsNext = GameObject.Find("StatsNextButton").GetComponent<Button>();
-        _buttonsNext = GameObject.Find("ButtonsNextButton").GetComponent<Button>();
-        _spellsNext = GameObject.Find("SpellsNextButton").GetComponent<Button>();
-        _infoNext = GameObject.Find("InfoNextButton").GetComponent<Button>();
         _tutorialCanvas = GameObject.Find("Tutorial").GetComponent<Canvas>();
+        _introCanvas = GameObject.Find("IntroCanvas").GetComponent<Canvas>();
         _statsCanvas = GameObject.Find("StatsCanvas").GetComponent<Canvas>();
         _buttonsCanvas = GameObject.Find("ButtonsCanvas").GetComponent<Canvas>();
         _spellsCanvas = GameObject.Find("SpellsCanvas").GetComponent<Canvas>();
         _infoCanvas = GameObject.Find("InfoCanvas").GetComponent<Canvas>();
+        _healthImages = GameObject.Find("HealthImages");
+        _manaImages = GameObject.Find("ManaImages");
+        _moveButtonImages = GameObject.Find("MoveButtonImages");
+        _spellButtonImages = GameObject.Find("SpellButtonImages");
+        _endTurnButtonImages = GameObject.Find("EndTurnButtonImages");
+        _spellsImages = GameObject.Find("SpellsImages");
+        _spellsInfoImages = GameObject.Find("SpellsInfoImages");
+        _healthText = GameObject.Find("HealthText");
+        _manaText = GameObject.Find("ManaText");
+        _manaImages.SetActive(false);
+        _manaText.SetActive(false);
     }
 
     void Start()
     {
-        
     }
     
     void Update()
@@ -49,33 +62,69 @@ public class TutorialController : MonoBehaviour
         _tutorialCanvas.enabled = false;
     }
 
+    public void IntroNext()
+    {
+        _introCanvas.enabled = false;
+        _statsCanvas.enabled = true;
+    }
+
     public void StatsNext()
     {
         _onClickCount++;
-        if (_onClickCount == 2)
+        switch (_onClickCount)
         {
-            _buttonsCanvas.enabled = true;
-            _onClickCount = 0;
+            case 1:
+                _healthImages.SetActive(false);
+                _manaImages.SetActive(true);
+                _manaText.SetActive(true);
+                break;
+            case 2:
+                _statsCanvas.enabled = false;
+                _buttonsCanvas.enabled = true;
+                _onClickCount = 0;
+                break;
         }
     }
     
     public void ButtonsNext()
     {
-        if (_onClickCount == 3)
+        _onClickCount++;
+        switch (_onClickCount)
         {
-            _spellsCanvas.enabled = true;
-            _buttonsCanvas.enabled = false;
-            _onClickCount = 0;
+            case 1:
+                _moveButtonImages.SetActive(false);
+                _moveButtonText.SetActive(false);
+                _spellButtonImages.SetActive(true);
+                _spellButtonText.SetActive(true);
+                break;
+            case 2:
+                _spellButtonImages.SetActive(false);
+                _spellButtonText.SetActive(false);
+                _endTurnButtonImages.SetActive(true);
+                _endTurnButtonText.SetActive(true);
+                break;
+            case 3:
+                _spellsCanvas.enabled = true;
+                _buttonsCanvas.enabled = false;
+                _onClickCount = 0;
+                break;
         }
     }
     
     public void SpellsNext()
     {
-        if (_onClickCount == 2)
+        _onClickCount++;
+        switch (_onClickCount)
         {
-            _spellsCanvas.enabled = false;
-            _infoCanvas.enabled = true;
-            _onClickCount = 0;
+            case 1:
+                _spellsImages.SetActive(false);
+                _spellsInfoImages.SetActive(true);
+                break;
+            case 2:
+                _spellsCanvas.enabled = false;
+                _infoCanvas.enabled = true;
+                _onClickCount = 0;
+                break;
         }
     }
     
